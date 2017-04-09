1f352fb158b4850fdd179614ca5a0b68709ea16a
ajlopez <webmaster@ajlopez.com>
2017-04-09 13:56:48 -0300

    First Neuron with zero value

---
+++ Src/Neurum.Tests/NeuronTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Neurum.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class NeuronTests
+    {
+        [TestMethod]
+        public void CreatedNeuronHasZeroValue()
+        {
+            Neuron neuron = new Neuron();
+
+            Assert.AreEqual(0, neuron.Value);
+        }
+    }
+}
---
+++ Src/Neurum/Neuron.cs
@@ -0,0 +1,12 @@
+﻿namespace Neurum
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Neuron
+    {
+        public double Value { get { return 0; } }
+    }
+}
