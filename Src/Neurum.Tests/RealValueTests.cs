3e690545ca9da404e9ef0c606215a7707cd321de
ajlopez <webmaster@ajlopez.com>
2017-04-09 14:14:39 -0300

    RealValue

---
+++ Src/Neurum.Tests/RealValueTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Neurum.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class RealValueTests
+    {
+        [TestMethod]
+        public void RealValueZero()
+        {
+            RealValue value = new RealValue();
+
+            Assert.AreEqual(0, value.Value);
+        }
+
+        [TestMethod]
+        public void RealValueWithValue()
+        {
+            RealValue value = new RealValue(3.14159);
+
+            Assert.AreEqual(3.14159, value.Value);
+        }
+    }
+}
---
+++ Src/Neurum/RealValue.cs
@@ -0,0 +1,35 @@
+﻿namespace Neurum
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class RealValue : IValue
+    {
+        private double value;
+
+        public RealValue()
+            : this(0)
+        {
+        }
+
+        public RealValue(double value)
+        {
+            this.value = value;
+        }
+
+        public double Value
+        {
+            get
+            {
+                return this.value;
+            }
+
+            set
+            {
+                this.value = value;
+            }
+        }
+    }
+}
