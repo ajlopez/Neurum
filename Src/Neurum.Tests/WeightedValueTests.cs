f35c46e44eeadf40e0138f6ba6e23cf6fde9910e
ajlopez <webmaster@ajlopez.com>
2017-04-10 16:55:14 -0300

    Weighted Value

---
+++ Src/Neurum.Tests/WeightedValueTests.cs
@@ -0,0 +1,51 @@
+﻿namespace Neurum.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class WeightedValueTests
+    {
+        [TestMethod]
+        public void CreatedWeightedValueWithValueZero()
+        {
+            var value = new WeigthedValue(new IValue[] { }, new double[] { });
+
+            Assert.IsNotNull(value.Inputs);
+            Assert.AreEqual(0, value.Inputs.Count);
+
+            Assert.IsNotNull(value.Weights);
+            Assert.AreEqual(0, value.Weights.Count);
+
+            Assert.AreEqual(0, value.Value);
+        }
+
+        [TestMethod]
+        public void CreatedWeightedValueWithValueOne()
+        {
+            var value = new WeigthedValue(new IValue[] { new RealValue(1) }, new double[] { 1 });
+
+            Assert.IsNotNull(value.Inputs);
+            Assert.AreEqual(1, value.Inputs.Count);
+
+            Assert.IsNotNull(value.Weights);
+            Assert.AreEqual(1, value.Weights.Count);
+
+            Assert.AreEqual(1, value.Value);
+        }
+
+        [TestMethod]
+        public void CreatedWeightedValueWithValueThree()
+        {
+            var value = new WeigthedValue(new IValue[] { new RealValue(1), new RealValue(2) }, new double[] { 1, 1 });
+
+            Assert.IsNotNull(value.Inputs);
+            Assert.AreEqual(2, value.Inputs.Count);
+
+            Assert.IsNotNull(value.Weights);
+            Assert.AreEqual(2, value.Weights.Count);
+
+            Assert.AreEqual(3, value.Value);
+        }
+    }
+}
---
+++ Src/Neurum/WeigthedValue.cs
@@ -0,0 +1,38 @@
+﻿namespace Neurum
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class WeigthedValue : IValue
+    {
+        private IList<IValue> inputs;
+        private IList<double> weights;
+
+        public WeigthedValue(IList<IValue> inputs, IList<double> weights)
+        {
+            this.inputs = inputs;
+            this.weights = weights;
+        }
+
+        public IList<IValue> Inputs { get { return this.inputs; } }
+
+        public IList<double> Weights { get { return this.weights; } }
+
+        public double Value
+        {
+            get
+            {
+                double result = 0;
+
+                int ninputs = this.inputs.Count;
+
+                for (int k = 0; k < ninputs; k++)
+                    result += this.inputs[k].Value * this.weights[k];
+
+                return result;
+            }
+        }
+    }
+}
