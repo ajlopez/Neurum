669743a744e9a6527b9fb6a08b8a5f2a0781e7d3
ajlopez <webmaster@ajlopez.com>
2017-04-14 13:03:09 -0300

    Neuron List first test

---
+++ Src/Neurum.Tests/NeuronListTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Neurum.Tests
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class NeuronListTests
+    {
+        [TestMethod]
+        public void CreatedNeuronList()
+        {
+            NeuronList list = new NeuronList(42);
+
+            Assert.IsNotNull(list.Neurons);
+            Assert.AreEqual(42, list.Neurons.Count);
+
+            for (int k = 0; k < 42; k++)
+                Assert.IsNotNull(list.Neurons[k]);
+        }
+    }
+}
---
+++ Src/Neurum/NeuronList.cs
@@ -0,0 +1,22 @@
+﻿namespace Neurum
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class NeuronList
+    {
+        private IList<Neuron> neurons;
+
+        public NeuronList(int nneurons)
+        {
+            this.neurons = new Neuron[nneurons];
+
+            for (int k = 0; k < nneurons; k++)
+                this.neurons[k] = new Neuron();
+        }
+
+        public IList<Neuron> Neurons { get { return this.neurons; } }
+    }
+}
