0ed3e9801ca4e09dd38453623ee9ed32e452327b
ajlopez <webmaster@ajlopez.com>
2017-04-29 09:06:33 -0300

    Neuron Layered Network, WIP

---
+++ Src/Neurum.Tests/NeuronLayeredNetworkTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Neurum.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class NeuronLayeredNetworkTests
+    {
+        [TestMethod]
+        public void CreateLayeredNetworkWithThreeLayers()
+        {
+            NeuronLayeredNetwork network = new NeuronLayeredNetwork(new int[] { 4, 5, 3 });
+
+            Assert.AreEqual(3, network.NLayers);
+            Assert.IsNotNull(network.GetLayer(0));
+            Assert.AreEqual(4, network.GetLayer(0).Count);
+            Assert.IsNotNull(network.GetLayer(1));
+            Assert.AreEqual(5, network.GetLayer(1).Count);
+            Assert.IsNotNull(network.GetLayer(2));
+            Assert.AreEqual(3, network.GetLayer(2).Count);
+        }
+    }
+}
---
+++ Src/Neurum/NeuronLayeredNetwork.cs
@@ -0,0 +1,23 @@
+﻿namespace Neurum
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class NeuronLayeredNetwork
+    {
+        private IList<IList<IValue>> layers;
+
+        public NeuronLayeredNetwork(IList<int> arities)
+        {
+        }
+
+        public int NLayers { get { return this.layers.Count; } }
+
+        public IList<IValue> GetLayer(int n)
+        {
+            return this.layers[n];
+        }
+    }
+}
