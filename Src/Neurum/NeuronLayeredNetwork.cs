c69e3741745d6a49e5273fb691f799751cfa23a9
ajlopez <webmaster@ajlopez.com>
2017-04-29 14:30:52 -0300

    Creating NeuronLayeredNetwork

--- Src/Neurum/NeuronLayeredNetwork.cs
+++ Src/Neurum/NeuronLayeredNetwork.cs
@@ -7,13 +7,27 @@
 
     public class NeuronLayeredNetwork
     {
-        private IList<IList<IValue>> layers;
+        private IList<IValue>[] layers;
 
         public NeuronLayeredNetwork(IList<int> arities)
         {
+            this.layers = new IList<IValue>[arities.Count];
+
+            this.layers[0] = new List<IValue>();
+
+            for (int k = 0; k < arities[0]; k++)
+                this.layers[0].Add(new RealValue());
+
+            for (int k = 1; k < arities.Count; k++)
+            {
+                this.layers[k] = new List<IValue>();
+
+                for (int j = 0; j < arities[k]; j++)
+                    this.layers[k].Add(new Neuron(WeightedValue.CreateWeightedValue(this.layers[k - 1])));
+            }
         }
 
-        public int NLayers { get { return this.layers.Count; } }
+        public int NLayers { get { return this.layers.Length; } }
 
         public IList<IValue> GetLayer(int n)
         {
