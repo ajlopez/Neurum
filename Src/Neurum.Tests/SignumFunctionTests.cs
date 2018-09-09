92beffdcd0c7a23d5306b0bfc6830e3f7976b127
ajlopez <webmaster@ajlopez.com>
2018-09-09 17:23:44 -0300

    Signum function

---
+++ Src/Neurum.Tests/SignumFunctionTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Neurum.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class SignumFunctionTests
+    {
+        [TestMethod]
+        public void Evaluate()
+        {
+            SignumFunction fn = new SignumFunction();
+
+            Assert.AreEqual(1.0, fn.Evaluate(1.0));
+            Assert.AreEqual(1.0, fn.Evaluate(1.1));
+            Assert.AreEqual(1.0, fn.Evaluate(0.1));
+            Assert.AreEqual(1.0, fn.Evaluate(1000));
+
+            Assert.AreEqual(-1.0, fn.Evaluate(-0.1));
+            Assert.AreEqual(-1.0, fn.Evaluate(-0.5));
+
+            Assert.AreEqual(0.0, fn.Evaluate(0.0));
+
+            Assert.AreEqual(-1.0, fn.Evaluate(-1.0));
+            Assert.AreEqual(-1.0, fn.Evaluate(-1.1));
+            Assert.AreEqual(-1.0, fn.Evaluate(-1000));
+        }
+    }
+}
---
+++ Src/Neurum/SignumFunction.cs
@@ -0,0 +1,15 @@
+﻿namespace Neurum
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class SignumFunction : IValueFunction
+    {
+        public double Evaluate(double value)
+        {
+            return value < 0.0 ? -1.0 : (value > 0.0 ? 1.0 : 0.0);
+        }
+    }
+}
